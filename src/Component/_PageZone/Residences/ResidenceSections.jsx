1150f177b934f24bdcd0baa19a502a942998484e
priya220719 <priya@innowrap.com>
2023-09-26 18:35:08 +0530

    UI: residence and villa page images change

--- src/Component/_PageZone/Residences/ResidenceSections.jsx
+++ src/Component/_PageZone/Residences/ResidenceSections.jsx
@@ -8,14 +8,14 @@ import LocationBenefits from '@/Component/_Sections/LocationBenefits/LocationBen
 
 export default function ResidenceSections() {
     const BannerTitle = 'Venice RESIDENCES';
-    const BannerImage = '/Assets/Residence/images/Banner.jpg';
+    const BannerImage = '/Assets/Residence/images/Banner.webp';
     const IntroSectionPara = 'Discover a revitalizing place to call home, where you wake up every day in an incredible waterfront paradise. With a gleaming Crystal Lagoon, pristine beachfront, vibrant boulevard, and world-class entertainment just steps away, this is a brand-new approach to apartment living, meticulously designed for the discerning few.';
     const IntroSectionCarouselImages = [
-        '/Assets/Residence/images/Carousel-Gallery-1.jpg',
-        '/Assets/Residence/images/Carousel-Gallery-2.jpg',
-        '/Assets/Residence/images/Carousel-Gallery-3.jpg',
-        '/Assets/Residence/images/Carousel-Gallery-4.jpg',
-        '/Assets/Residence/images/Carousel-Gallery-5.jpg'
+        '/Assets/Residence/images/Carousel-Gallery-1.webp',
+        '/Assets/Residence/images/Carousel-Gallery-2.webp',
+        '/Assets/Residence/images/Carousel-Gallery-3.webp',
+        '/Assets/Residence/images/Carousel-Gallery-4.webp',
+        '/Assets/Residence/images/Carousel-Gallery-5.webp'
     ]
     const ExquisiteDetailsPara = 'Every detail matters in this thoughtfully crafted environment, where meticulous architecture and premium design harmonize with the natural surroundings. Floor-to-ceiling windows seamlessly blend indoor and outdoor spaces, offering breathtaking views of the water and cascading waterfalls. Every detail impeccable, every material exquisite.';
     const LifestylePara = 'Experience a captivating lifestyle where your every desire is effortlessly fulfilled. Our apartment complexes are meticulously designed to encompass everything you need, including a state-of-the-art gym, steam room, sauna, your own cinema, and a clubhouse.';
@@ -26,7 +26,7 @@ export default function ResidenceSections() {
         'State-of-the-art WORKOUT',
         'WILD AND FREE'
     ]
-    const LifestyleImage = '/Assets/Residence/images/life-style.jpg';
+    const LifestyleImage = '/Assets/Residence/images/life-style.webp';
     const LifestyleImageTitle = 'POOLSIDE PERFECTION';
     const LocationBenefitsTitle = 'LOCATION BENEFITS';
     const LocationBenefitsPara = 'Located in Dubai South, the city`s most expansive singular urban master development. Azizi Venice will become a part of a smart and futuristic metropolis that promises entirely new standard of living. Over 136 hectares in size, Azizi Venice is set to be a prestigious yet well-connected enclave.';
--- src/Component/_PageZone/Villas/VillasSections.jsx
+++ src/Component/_PageZone/Villas/VillasSections.jsx
@@ -7,16 +7,16 @@ import LocationBenefits from '@/Component/_Sections/LocationBenefits/LocationBen
 
 export default function VillasSections() {
     const BannerTitle = 'Venice VILLAS';
-    const BannerImage = '/Assets/Villa/images/Banner.jpg';
+    const BannerImage = '/Assets/Villa/images/Banner.webp';
     const IntroSectionPara = 'Tucked away in a charming, gated community along the crystal-blue lagoon, you`ll find Azizi Venice Villas - meticulously designed for those who savor life`s finer moments. Returning home to your Azizi Venice Villa is a lavish experience like no other in Dubai.';
     const IntroSectionCarouselImages = [
-        '/Assets/Villa/images/Carousel-Gallery-1.jpg',
-        '/Assets/Villa/images/Carousel-Gallery-2.jpg',
-        '/Assets/Villa/images/Carousel-Gallery-3.jpg',
-        '/Assets/Villa/images/Carousel-Gallery-4.jpg',
-        '/Assets/Villa/images/Carousel-Gallery-5.jpg',
-        '/Assets/Villa/images/Carousel-Gallery-6.jpg',
-        '/Assets/Villa/images/Carousel-Gallery-7.jpg'
+        '/Assets/Villa/images/Carousel-Gallery-1.webp',
+        '/Assets/Villa/images/Carousel-Gallery-2.webp',
+        '/Assets/Villa/images/Carousel-Gallery-3.webp',
+        '/Assets/Villa/images/Carousel-Gallery-4.webp',
+        '/Assets/Villa/images/Carousel-Gallery-5.webp',
+        '/Assets/Villa/images/Carousel-Gallery-6.webp',
+        '/Assets/Villa/images/Carousel-Gallery-7.webp'
     ]
     const ExquisiteDetailsPara = 'From the use of biophilic materials and serene color schemes to cutting-edge technology with exceptional functionality, each villa radiates an aura of refined elegance and tranquil grandeur.';
     const LifestylePara = 'Pamper yourself with an array of luxurious amenities designed to enhance your daily life. Each villa is a modern sanctuary, featuring a private elevator, home cinema, games room, personal infinity pool, opulent rooftop terrace, and a BBQ area for entertaining. Plus, you`ll enjoy direct, effortless access to the swimmable crystal lagoon, just steps from your doorstep.';
@@ -28,7 +28,7 @@ export default function VillasSections() {
         'WINTER WONDERLAND',
         'EXPANSIVE VIEWS'
     ]
-    const LifestyleImage = '/Assets/Villa/images/life-style.jpg';
+    const LifestyleImage = '/Assets/Villa/images/life-style.webp';
     const LifestyleImageTitle = 'ELEVATED ENTERTAINMENT';
     const LocationBenefitsTitle = 'LOCATION BENEFITS';
     const LocationBenefitsPara = 'Located in Dubai South, the city`s most expansive singular urban master development. Azizi Venice will become a part of a smart and futuristic metropolis that promises entirely new standard of living. Over 136 hectares in size, Azizi Venice is set to be a prestigious yet well-connected enclave.';

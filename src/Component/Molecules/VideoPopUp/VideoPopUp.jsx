013df88ac63a57b4e26126d721fca534f2d05d47
Abhishek Rai <upscalesalestech@gmail.com>
2023-09-28 14:17:30 +0530

    Fix: Popvideo responsiveness

--- src/Component/Molecules/VideoPopUp/VideoPopUp.jsx
+++ src/Component/Molecules/VideoPopUp/VideoPopUp.jsx
@@ -9,13 +9,7 @@ const style = {
     top: '50%',
     left: '50%',
     width: '80vw',
-    height: '50%',
     transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    // border: '2px solid #000',
-    // boxShadow: 24,
-    backGround: '#fff'
-    // p: 4,
 };
 export default function VideoPopUp() {
     const [open, setOpen] = useState(false);
